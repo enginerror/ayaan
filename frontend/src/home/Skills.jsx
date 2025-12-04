e28698a247dede6ea439949cf82df13b9ae0600a
enginerror <ayaanmurshedkhan@gmail.com>
2025-12-04 23:15:22 +0530

    reconstruct

--- frontend/src/home/Skills.jsx
+++ frontend/src/home/Skills.jsx
@@ -1,5 +1,4 @@
-import SkillsIcon from "@/components/SkillIcon";
-import ThinLine from "@/components/ThinLine";
+import SkillsIcon from "@/components/Icon";
 
 const Skills = () => {
   const skills = [
@@ -17,7 +16,7 @@ const Skills = () => {
   return (
     <div className="h-[40vh] mt-6 sm:mt-8 md:mt-10 pb-8 sm:pb-12">
       <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-6 md:mb-8 lg:mb-10 prata-regular flex justify-center items-center gap-3">
-        Skills <ThinLine />
+        Skills
       </h1>
 
       <div className="flex items-center justify-center flex-wrap gap-1 md:gap-1.5 lg:mx-80 md:mt-6 cursor-default">
--- src/components/ThinLine.jsx
+++
@@ -1,7 +0,0 @@
-const ThinLine = () => {
-  return (
-    <div className="hidden sm:block w-4 sm:w-8 md:w-12 lg:w-14 h-[1.5px] bg-foreground rounded-sm"></div>
-  );
-};
-
-export default ThinLine;
